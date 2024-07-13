2563aca4717bc9dda667469a6e9b207866a11836
Khan Ebrahim <150028854+iideviibrahim@users.noreply.github.com>
2024-07-13 16:45:56 +0600

    create profileinfocard

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -9,7 +9,7 @@ export const ProfileScreen = () => {
       <ProfileInfoCard
         name="Daniel Quite"
         username="paypeoples.com/@usernaem"
-        profileImage={require('../..//assets/images/profile.jpeg')}
+        profileImage={require('../../assets/images/profile.jpeg')}
         ratings={[1, 1, 0, 0, 0]}
       />
     </Screen>
